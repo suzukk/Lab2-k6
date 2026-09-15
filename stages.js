6867cae33120dd11a3bb7119aa49df2f045d41b8
suzukk <tergelsurenkhuu@gmail.com>
2026-09-15 22:39:21 +0800

    Staged test

---
+++ stages.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '30s', target: 5 },
+    { duration: '1m', target: 30 },
+    { duration: '30s', target: 100 },
+    { duration: '30s', target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+
+  check(res, {
+    'status 200 байна': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
